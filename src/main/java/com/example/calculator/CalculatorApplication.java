5ee5546138630511dfb0ed13823a3b49a0516bb9
air <kobychev00@bk.ru>
2023-04-17 19:27:20 +0300

    2

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
     public static void main(String[] args) {
+        System.out.println("1");
         SpringApplication.run(CalculatorApplication.class, args);
     }
 
